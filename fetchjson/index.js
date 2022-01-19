8c73603637ac73dfebe273cc872acd176427f1ae
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-01-19 00:11:41 -0800

    9. Catching Errors with Typescript

--- fetchjson/index.js
+++ fetchjson/index.js
@@ -3,5 +3,10 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    // console.log(response.data)
+    var todo = response.data;
+    var ID = todo.ID;
+    var title = todo.Title;
+    var finished = todo.finished;
+    console.log("\n      The todo with ID : ".concat(ID, ";\n      Has a Title: ").concat(title, ";\n      Has it been finished? ").concat(finished, ";\n  "));
 });
--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -1,7 +1,26 @@
 import axios from 'axios';
 
+interface Todo {
+  id: number;
+  titler: string;
+  finished: boolean;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 axios.get(url).then(response => {
-  console.log(response.data)
+  // console.log(response.data)
+
+  const todo = response.data;
+
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+      The todo with ID : ${ID};
+      Has a Title: ${title};
+      Has it been finished? ${completed};
+  `)
+
 });
